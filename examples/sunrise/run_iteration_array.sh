1f13bf2e914d2a76ac80a63e8b3519af4fb9caca
Juan Rodríguez Pérez <68055182+DrakeWhu@users.noreply.github.com>
2026-07-31 14:43:20 +0200

    Remove memory limit from iteration arrays

--- examples/sunrise/run_iteration_array.sh
+++ examples/sunrise/run_iteration_array.sh
@@ -5,7 +5,6 @@
 #SBATCH --ntasks=24
 #SBATCH --ntasks-per-node=24
 #SBATCH --cpus-per-task=1
-#SBATCH --mem=64G
 #SBATCH --time=06:00:00
 #SBATCH --array=0-0
 #SBATCH --output=loop_logs/%x_%A_%a.out
